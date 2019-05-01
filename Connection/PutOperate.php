dac0a077fce4f4e96b5fbf3cb4d06b695d248d20
yang <yangqingxian@niaoni.la>
2019-05-01 19:59:02 +0800

    将底层操作类封装完成

--- Connection/PutOperate.php
+++ Connection/PutOperate.php
@@ -15,5 +15,32 @@ namespace OwnCloudeSDK\Connection;
 
 class PutOperate extends Base
 {
-    
+    public function put($url,$filePath,$isHttps){
+        $fullUrl=$this->getUrlPrefix($url,$isHttps);
+        $body=file_get_contents($filePath);
+        if(!$body){
+            return self::returnResult(false,"文件地址错误");
+        }
+        $header=array(
+            'Content-Type'=>"multiple/form-data"
+        );
+        try{
+            $client=self::getClient();
+            $response=$client->request(
+                'PUT',
+                $fullUrl,
+                [
+                    'header'=>$header,
+                    'body'=>$body
+                ]
+            );
+            $returnStatus=intval($response->getStatusCode());
+            if($returnStatus==204){
+                return self::returnResult(false,"重名文件已存在");
+            }
+            return self::returnResult();
+        }catch (\Exception $e){
+            return self::returnResult(false,$e->getMessage());
+        }
+    }
 }
