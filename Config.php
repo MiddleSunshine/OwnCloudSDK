f907a9ff391c24c8de9922ed4bf664edabdd4323
yang <yangqingxian@niaoni.la>
2019-05-01 21:35:34 +0800

    修改配置值

--- Config.php
+++ Config.php
@@ -10,7 +10,8 @@
  *   Modification
  */
 return array(
-    'domain'=>"",
-    'user_name'=>"",
-    'password'=>""
+    'domain'=>"pdtowncloud.pewinner.com",
+    'user_name'=>"admin",
+    'password'=>"admin",
+    'is_https'=>true
 );
